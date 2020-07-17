93d7ec14a366e4edd754b951362714b475570258
AasthaVarma <34837646+AasthaVarma@users.noreply.github.com>
2020-07-17 22:07:30 +0530

    Add files via upload

---
+++ test_project/isim/test_krtsba_top_isim_beh.exe.sim/work/m_00000000002017169483_2326951613.c
@@ -0,0 +1,55 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0xa0883be4 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "D:/drdo/Elliptic_curve_pro/test_krtsba_top.v";
+static unsigned int ng1[] = {5000U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U};
+static unsigned int ng2[] = {5U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U, 0U};
+
+
+
+static void Initial_41_0(char *t0)
+{
+    char *t1;
+    char *t2;
+
+LAB0:    xsi_set_current_line(41, ng0);
+
+LAB2:    xsi_set_current_line(44, ng0);
+    t1 = ((char*)((ng1)));
+    t2 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t2, t1, 0, 0, 233);
+    xsi_set_current_line(45, ng0);
+    t1 = ((char*)((ng2)));
+    t2 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t2, t1, 0, 0, 233);
+
+LAB1:    return;
+}
+
+
+extern void work_m_00000000002017169483_2326951613_init()
+{
+	static char *pe[] = {(void *)Initial_41_0};
+	xsi_register_didat("work_m_00000000002017169483_2326951613", "isim/test_krtsba_top_isim_beh.exe.sim/work/m_00000000002017169483_2326951613.didat");
+	xsi_register_executes(pe);
+}
